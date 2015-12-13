37b7a33a953d537ea3b7f468c15fcd9c5911a07a
lakerolis <jorgenfalch@gmail.com>
2015-12-13 18:15:31 +0100

    more zombies v2

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			Debug.Break ();
+			return;
+		}
+
+		if (false)//other.gameObject.transform.parent) 
+		{
+			Destroy (other.gameObject.transform.parent.gameObject);
+		} 
+		else 
+		{
+			Destroy (other.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
